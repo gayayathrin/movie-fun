197622ae8f3e3a341e13b198a4ba5d76054a8ca8
gayathrin <gayathri.nelavigi@wipro.com>
2018-10-15 13:51:07 +0900

    spring bootification

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -1,7 +1,8 @@
 package org.superbiz.moviefun;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
-
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -9,5 +10,9 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    @Bean
+    public ServletRegistrationBean actionServletRegistration(ActionServlet actionServlet) {
+        return new ServletRegistrationBean(actionServlet, "/moviefun/*");
     }
 }
